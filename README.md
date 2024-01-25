2860f7485f31444594af3da8c9ec5b4153f4cf42
Ansh <26479077+AnshGupta01@users.noreply.github.com>
2024-01-25 18:45:12 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# Task Management Application
+A simple Task Management application with Node.js, Express.js, and MongoDB for data persistence.
+
+### Prerequisites
+- Node.js
+- npm
+- MongoDB
+
+### Installation
+
+1. Clone the repository:
+   `git clone https://github.com/AnshGupta01/Task-manager.git`
+2. Navigate to the directory
+    `cd "folder_name"`
+3. Install the dependencies
+    `npm install`
+4. Create a .env file in the project root with your MongoDB connection string:
+    `MONGODB_URI=mongodb://localhost:27017/todoapp`
+
+### Usage
+Simply run with `npm start`
+The application will be accessible at `http://localhost:3000`.
+
+### API Usage
+The API has the following endpoints:
+
+1. `GET /tasks`: Get all tasks.
+2. `GET /tasks/category/:category`: Get all tasks with defined category.
+3. `POST /tasks`: Create a new task.
+4. `PUT /tasks/:id`: Update a task.
+5. `PUT /tasks/:id/complete`: Mark a task as completed.
+6. `DELETE /tasks/:id`: Delete a task.
+
+
+### Code Structure & Key Decisions
+
+1. **Dependencies:**
+   - The project uses the following dependencies:
+     - `express`: A popular web framework for Node.js.
+     - `mongoose`: An ODM (Object-Document Mapper) for MongoDB, simplifying interaction with the database.
+     - `body-parser`: Middleware for parsing JSON requests.
+     - `dotenv`: To process the env file
+
+2. **Environment Variables:**
+   - The MongoDB connection string is stored in a `.env` file, and the application uses the `dotenv` package to load environment variables.
+
+3. **Database Model:**
+   - A simple `Task` model is defined using Mongoose, representing tasks in the MongoDB collection. It includes fields for `title`, `description`,`completed` and `dueDate`.
+
+4. **EndPoints:**
+     - `GET /tasks`: Retrieve all tasks.
+     - `GET /tasks/category/:category`: Get all tasks with defined category.
+     - `POST /tasks`: Create a new task.
+     - `PUT /tasks/:id`: Update a task.
+     - `PUT /tasks/:id/complete`: Mark a task as completed.
+     - `DELETE /tasks/:id`: Delete a task.
+
+5. **Error Handling:**
+   - The code includes error handling for database operations and provides meaningful error messages in case of failures.
+
+6. **Validation:**
+   - Basic validation is implemented to ensure that task titles are not empty.
+   - Users cannot mark a task as complete if it's already marked as such.
+
+7. **README:**
+   - The `README.md` file provides clear instructions on how to set up and use the application. 
---
+++ app.js
@@ -0,0 +1,140 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const app = express();
+const port = 3000;
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on(
+  "error",
+  console.error.bind(console, "MongoDB connection error:")
+);
+
+// Task model with mongoose
+const Task = mongoose.model("Task", {
+  title: String,
+  description: String,
+  completed: Boolean,
+  dueDate: Date,
+  category: String,
+});
+
+// Middleware for  JSON requests
+app.use(bodyParser.json());
+
+// GET All Tasks
+app.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.json(tasks);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+//GET tasks by category
+app.get('/tasks/category/:category', async (req, res) => {
+    const category = req.params.category;
+  
+    try {
+      const tasksByCategory = await Task.find({ category });
+      res.json(tasksByCategory);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+  
+// POST a new task
+app.post("/tasks", async (req, res) => {
+  const { title, description, dueDate, category } = req.body;
+
+  if (!title) {
+    return res.status(400).json({ error: "Title cannot be empty" });
+  }
+
+  try {
+    const task = new Task({
+      title,
+      description,
+      completed: false,
+      dueDate,
+      category,
+    });
+    await task.save();
+    res.json(task);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// PUT an existing task
+app.put("/tasks/:id", async (req, res) => {
+  const taskId = req.params.id;
+  const { title, description, completed, dueDate, category } = req.body;
+
+  try {
+    const task = await Task.findByIdAndUpdate(
+      taskId,
+      { title, description, completed, dueDate, category },
+      { new: true }
+    );
+
+    if (!task) {
+      return res.status(404).json({ error: "Task not found" });
+    }
+
+    res.json(task);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// PUT an existing task (tO Mark it complete)
+app.put("/tasks/:id/complete", async (req, res) => {
+  const taskId = req.params.id;
+
+  try {
+    const task = await Task.findById(taskId);
+    if (!task) {
+      return res.status(404).json({ error: "Task not found" });
+    }
+
+    // Check if the task is already completed
+    if (task.completed) {
+      return res.status(400).json({ error: "Task is already completed" });
+    }
+
+    // Mark the task as completed
+    task.completed = true;
+    await task.save();
+
+    res.json(task);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// DELETE endpoint to delete any task
+app.delete("/tasks/:id", async (req, res) => {
+  const taskId = req.params.id;
+
+  try {
+    const task = await Task.findByIdAndDelete(taskId);
+    if (!task) {
+      return res.status(404).json({ error: "Task not found" });
+    }
+    res.json({ success: true });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`);
+});
